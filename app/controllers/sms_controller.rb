c4da2d6b404411b4e9dee1d56618561399547220
asime <asime@opensourceconnections.com>
2008-10-18 09:35:27 -0400

    adding in test sms page

---
+++ app/controllers/sms_controller.rb
@@ -0,0 +1,92 @@
+class SmsController < ApplicationController
+  # GET /sms
+  # GET /sms.xml
+  def index
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @sms }
+    end
+  end
+
+  # GET /sms/send_sms
+  # GET /sms/send_sms.xml
+  def send_sms
+    
+    render :text => "hello sms test submit"
+    
+  end
+
+  # GET /sms/1
+  # GET /sms/1.xml
+  def show
+    @sms = Sms.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @sms }
+    end
+  end
+
+  # GET /sms/new
+  # GET /sms/new.xml
+  def new
+    @sms = Sms.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @sms }
+    end
+  end
+
+  # GET /sms/1/edit
+  def edit
+    @sms = Sms.find(params[:id])
+  end
+
+  # POST /sms
+  # POST /sms.xml
+  def create
+    @sms = Sms.new(params[:sms])
+
+    respond_to do |format|
+      if @sms.save
+        flash[:notice] = 'Sms was successfully created.'
+        format.html { redirect_to(@sms) }
+        format.xml  { render :xml => @sms, :status => :created, :location => @sms }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @sms.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /sms/1
+  # PUT /sms/1.xml
+  def update
+    @sms = Sms.find(params[:id])
+
+    respond_to do |format|
+      if @sms.update_attributes(params[:sms])
+        flash[:notice] = 'Sms was successfully updated.'
+        format.html { redirect_to(@sms) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @sms.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sms/1
+  # DELETE /sms/1.xml
+  def destroy
+    @sms = Sms.find(params[:id])
+    @sms.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(sms_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
