82e67120f5307e452ebdd6fb63e30d3cb40a23f3
Eric Pugh <epugh@eric-pughs-macbook.local>
2008-10-19 11:28:48 -0400

    speccin

--- spec/models/treasure_spec.rb
+++ spec/models/treasure_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Treasure do
+  fixtures :users, :hunts
   before(:each) do
     @valid_attributes = {
       :name => "value for name",
@@ -15,6 +16,9 @@ describe Treasure do
       :points => "1",
       :key => "value for key"
     }
+    
+    @quentinshunt = hunts(:quentinshunt)
+    @quentin = users(:quentin)
   end
 
   it "should create a new instance given valid attributes" do
@@ -22,24 +26,24 @@ describe Treasure do
   end
   
   it "should be able to detect if a Discovery took place within a radius of a Treasure" do
-    t = Treasure.create!(:name => "Cool Treasure", :clue => "where Eric Pugh Lives", :lat => 38.012331, :lng => -78.514305, :proximity => 1000)
+    t = Treasure.create!(:hunt => @quentinshunt,:points => 10, :description => "dude",:name => "Cool Treasure", :clue => "where Eric Pugh Lives", :lat => 38.012331, :lng => -78.514305, :proximity => 1000)
     
-    d = Discovery.create!(:lat => 38.012411, :lng => -78.515275)
+    d = Discovery.create!(:hunt => @quentinshunt, :user => @quentin, :treasure => t,:lat => 38.012411, :lng => -78.515275)
     
     distance = Treasure.distance_between(d, t)
     
     t.proximate?(d).should be_true
     
-    rotunda = Discovery.create!(:lat => 38.035581, :lng => -78.503548)
+    rotunda = Discovery.create!(:hunt => @quentinshunt, :user => @quentin, :treasure => t,:lat => 38.035581, :lng => -78.503548)
     
     t.proximate?(rotunda).should be_false
     
   end
   
   it "should be able to detect if a Discovery took place within a radius of a Treasure part deux" do
-    t = Treasure.create!(:name => "Cool Treasure", :clue => "where Eric Pugh Lives", :lat => 70.2278, :lng => -85.5864, :proximity => 1000)
+    t = Treasure.create!(:hunt => @quentinshunt, :points => 5, :description => "Erics House", :name => "Cool Treasure", :clue => "where Eric Pugh Lives", :lat => 70.2278, :lng => -85.5864, :proximity => 1000)
     
-    d = Discovery.create!(:lat => 70.2278, :lng => -85.5864)
+    d = Discovery.create!(:hunt => @quentinshunt, :user => @quentin, :treasure => t, :lat => 70.2278, :lng => -85.5864)
     
     distance = Treasure.distance_between(d, t)
     
