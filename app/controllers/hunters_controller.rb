1c4e6d155b09bf06ea47d59620a9c8898129f4c1
Ashish Tonse <ashish@ashish-tonses-macbook.local>
2008-10-19 09:47:02 -0400

    fixing some merge issues

--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -1,10 +1,13 @@
 class HuntersController < ApplicationController
   def index
-    @hunts = Hunt.hunting.select{ |h| h.user != current_user }
+    @hunts = Hunt.hunting# .select{ |h| h.user != current_user }
     puts @hunts.inspect
     if (session[:hunt])
       redirect_to :action => :continue
     end
+  end
+  
+  def start_mobile
     
   end
   
@@ -14,7 +17,10 @@ class HuntersController < ApplicationController
   end
   
   def continue
-    # puts current_user
-    render  :text => current_user
+    if (session[:hunt])
+      render  :text => current_user
+    else
+      redirect_to :action => :index
+    end
   end
 end
