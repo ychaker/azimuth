4d8866d01b3ed675d4a682cc05f77d76e74b72b7
Youssef Chaker <youssefchaker@youssef-chakers-macbook.local>
2008-10-19 18:16:39 -0400

    user lat and lng

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,9 +77,9 @@ class UsersController < ApplicationController
   
   def create_new_user(attributes)
     @user = User.new(attributes)
-    ip = request.env['HTTP_X_CLUSTER_CLIENT_IP'].nil? ? request.remote_ip : request.env['HTTP_X_CLUSTER_CLIENT_IP']
+    ip = request.env['HTTP_X_CLUSTER_CLIENT_IP'].blank? ? request.remote_ip : request.env['HTTP_X_CLUSTER_CLIENT_IP']
     begin
-      @user.set_coord_from_maxmind ip
+      @user.set_coord_from_maxmind(ip)
     rescue LocationNotFound
     end
     if @user && @user.valid?
